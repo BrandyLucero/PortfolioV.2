26e19db2c6730e9ab5e2b48a7c429eae8bc9b165
Brandy C. Lucero <brandy.c.lucero@gmail.com>
2014-10-06 21:57:13 -0600

    10/06/14

---
+++ Portfolio/Portfolio/Models/ContactForm.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Portfolio.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class ContactForm
+    {
+        public int ContactID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string ContactNumber{get; set;}
+        public string Comments { get; set; }
+    }
+}
---
+++ Portfolio/Portfolio/Scripts/main.js
@@ -0,0 +1,62 @@
+﻿//implement the DOM to accept our code
+$(document).ready(function () {
+    ////carousel functions start
+    //$('.div1 > .image img').on('click', function () {
+    //    //get the active slide
+    //    var activeSlide = $('.image.active');
+    //    //get the next slide
+    //    var nextSlide = activeSlide.next();
+    //    //make sure its a carousel slide
+    //    if (!nextSlide.hasClass('image')) {
+    //        nextSlide = $('.image').first();
+    //    }
+    //    //remove the active class, add the hide class to the active slide
+    //    activeSlide.removeClass('active').addClass('hide');
+    //    //remove the hide class, add the active class to the active slide
+    //    nextSlide.removeClass('hide').addClass('active');
+    //});
+    ////carousel functions end
+
+    //create function to hold everything and set interval
+    setInterval(function () {
+        NextSlide('guess');
+    }, 5000);
+
+    setInterval(function () {
+        NextSlide('hang');
+    }, 5000);
+
+    setInterval(function () {
+        NextSlide('poker');
+    }, 5000);
+
+    setInterval(function () {
+        NextSlide('trivia');
+    }, 5000);
+
+    var NextSlide = function (name) {
+        var activeSlide = $('#' + name + ' .active');
+        //alert(activeSlide.attr('src'));
+        //get the next slide
+        if (!nextSlide.hasClass('image')) {
+            nextSlide = activeSlide.parent().find('.image').first();
+        }
+        activeSlide.removeClass('active').addClass('hide');
+        //remove the active class, add  the hide class to the active slide
+        //remove the hide class, add the active class to the active slide
+        nextSlide.removeClass('hide').addClass('active');
+    };
+    //checking for validity when submitting on contact page
+    $('#contactForm').on('submit', function (event) {
+        event.preventDefault();
+        if ($(this).valid())
+            $.post($(this).attr('action'), $(this).serialize(), function (date) {
+                $('#container').html(data);
+            });
+    });
+
+    //carousel for the games on work page
+    $('carousel-next').on('click', function () {
+        (this)
+    });
+});
